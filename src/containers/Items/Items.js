544df1e383e68d79fa086b2079d77517549a1fe4
Brian Lau <lauwangtatbrian@gmail.com>
2017-07-05 15:43:43 -0700

    initial commit

---
+++ src/containers/Items/Items.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+
+const Items = () => (
+  <h1>Heelow</h1>
+);
+
+export default Items;
---
+++ src/containers/Items/ItemsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Items from './Items';
+
+class ItemsContainer extends Component {
+  render() {
+    return (
+      <Items />
+    );
+  }
+}
+
+
+export default ItemsContainer;
