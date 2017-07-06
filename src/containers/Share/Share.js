cd1417154c345c3d1bdeb0bc3aa69d2ef2d67d68
Brian Lau <lauwangtatbrian@gmail.com>
2017-07-05 22:21:34 -0700

    add components and functions to Share

---
+++ src/containers/Share/Share.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+
+const Share = () => (
+  <h1>not found</h1>
+);
+
+export default Share;
---
+++ src/containers/Share/ShareContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Share from './Share';
+
+class ShareContainer extends Component {
+  render() {
+    return (
+      <Share />
+    );
+  }
+}
+
+
+export default ShareContainer;
