312317b64c1c322e298a80f8d9abf7c955417da3
Brian Lau <lauwangtatbrian@gmail.com>
2017-07-05 19:45:39 -0700

    initial commit

---
+++ src/containers/NotFound/NotFoundContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import NotFound from './NotFound';
+
+class ItemsContainer extends Component {
+  render() {
+    return (
+      <Items />
+    );
+  }
+}
+
+
+export default ItemsContainer;
