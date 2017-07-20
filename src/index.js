5fa8f406f4ce3a2ed7be2adccb670739bf1328d5
Brian Lau <lauwangtatbrian@gmail.com>
2017-07-20 14:27:03 -0700

    remove provider from redux, add apolloprovider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import registerServiceWorker from './registerServiceWorker';
